78be3e3a22c3a93e2e24410a2c7a3d28491e4980
bimogempar <bimogempar@gmail.com>
2022-08-05 20:52:13 +0700

    fix footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 export default function Footer() {
     return (
         <div className="mt-5 -mb-2 flex justify-center">
-            COK KIK IKI FOOTER E BENERNO KIK!!!!
+            <div className='text-[8px] opacity-50 text-center'>
+                <div>
+                    Ⓒ Copyright 2022 - All rights reserved
+                </div>
+                <div>
+                    Made with ♡ by Rudikelelawar
+                </div>
+            </div>
         </div>
     )
 }
