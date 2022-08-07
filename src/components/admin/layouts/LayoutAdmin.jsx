3bbae4f0d2c3c282f53f2d60c963ea07c41f22c2
RizkiB12 <rbagus306@gmail.com>
2022-08-07 18:40:03 +0700

    add props

--- src/components/admin/layouts/LayoutAdmin.jsx
+++ src/components/admin/layouts/LayoutAdmin.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 import NavbarAdmin from '../navbar/NavbarAdmin'
 import Footer from '../../footer/Footer'
 
-function LayoutAdmin() {
+function LayoutAdmin(props) {
     return (
         <div className='bg-gray-100 w-full'>
             <div>
                 <NavbarAdmin />
+                {props.children}
             </div>
             <Footer />
         </div>
