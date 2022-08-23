ae9fd1247543e16484fcd72070c78b599abff6b0
bimogempar <bimogempar@gmail.com>
2022-08-23 19:24:34 +0700

    remove footer rudikelelawar

--- src/components/navbar/BotttomNavbar.jsx
+++ src/components/navbar/BotttomNavbar.jsx
@@ -9,7 +9,7 @@ export default function BotttomNavbar() {
     const navigate = useNavigate()
 
     return (
-        <div className='flex justify-around items-center sticky bottom-0 -m-5 px-5 py-4 bg-gradient-to-l from-green-600 to-green-500 text-white text-xl'>
+        <div className='flex justify-around items-center sticky bottom-0 -m-5 px-5 py-4 mt-6 bg-gradient-to-l from-green-600 to-green-500 text-white text-xl'>
             <AiFillHome className='hover:scale-110 transition ease-in-out delay-150 cursor-pointer' onClick={() => navigate('/')} />
             <MdDashboard className='hover:scale-110 transition ease-in-out delay-150 cursor-pointer' onClick={() => navigate('/products')} />
             <IoMdListBox className='hover:scale-110 transition ease-in-out delay-150 cursor-pointer' onClick={() => navigate('/notification')} />
