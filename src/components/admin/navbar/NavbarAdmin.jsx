0e5840c45a94755337ed0b99f289634de5f4f995
bimogempar <bimogempar@gmail.com>
2022-09-10 16:06:21 +0700

    install toastify and swal2

--- src/components/admin/navbar/NavbarAdmin.jsx
+++ src/components/admin/navbar/NavbarAdmin.jsx
@@ -3,14 +3,27 @@ import { useDispatch } from 'react-redux'
 import { Navigate, NavLink } from 'react-router-dom'
 import { logout } from '../../../redux/slice/AuthSlice'
 import { FiLogOut } from 'react-icons/fi'
+import Swal from 'sweetalert2'
 
 function NavbarAdmin() {
     const [isOpen, setIsOpen] = useState(false)
     const dispatch = useDispatch()
 
     const handleLogout = () => {
-        dispatch(logout())
-        Navigate('/')
+        Swal.fire({
+            title: "Yakin logout?",
+            text: "Apakah anda yakin untuk log out?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "rgb(22 163 74)",
+            cancelButtonColor: "rgb(156 163 175)",
+            confirmButtonText: "Yes",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                dispatch(logout())
+                Navigate('/')
+            }
+        });
     }
 
     return (
@@ -36,7 +49,7 @@ function NavbarAdmin() {
                         <NavLink to="/admin/users" className='block px-4 py-2 lg:py-5 text-black hover:bg-green-600 hover:text-white'>Users</NavLink>
                     </div>
                     <div className='flex flex-col lg:flex-row'>
-                        <div onClick={handleLogout} className='cursor-pointer hover:bg-green-600 hover:text-white block px-4 py-2 lg:py-5 text-black hover:text-gray-500'><div className="flex items-center gap-3"><FiLogOut /> Logout</div></div>
+                        <div onClick={handleLogout} className='cursor-pointer hover:bg-green-600 hover:text-white block px-4 py-2 lg:py-5 text-black'><div className="flex items-center gap-3"><FiLogOut /> Logout</div></div>
                     </div>
                 </div>
             </div>
