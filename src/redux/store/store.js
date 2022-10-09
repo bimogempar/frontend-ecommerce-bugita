98cdcf0b79a9a544f2c7557e5caf8d3eb8d6e987
bimogempar <bimogempar@gmail.com>
2022-10-09 13:16:52 +0700

    update: done first setup redux persist for authUser

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,10 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
 import AuthSlice from "../slice/AuthSlice";
 import CartSlice from "../slice/CartSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import thunk from "redux-thunk";
 
-export default configureStore({
-    reducer: {
-        cart: CartSlice,
-        authUser: AuthSlice,
-    },
-});
+const persistConfig = {
+    key: "root",
+    storage,
+    whitelist: ["authUser"],
+}
+
+const rootReducer = combineReducers({
+    authUser: AuthSlice,
+    cart: CartSlice,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: [thunk],
+})
+
+export default store
